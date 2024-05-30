c88508b4619de286a6e2466b569ac7a830beb2bb
nghiata-dev <nghiata.dev@gmail.com>
2024-05-30 10:49:13 +0700

    add trigger

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App Trigger
+            App Trigg
           </Typography>
           <Button
             color="primary"
