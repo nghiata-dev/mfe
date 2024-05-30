00524284c3a36d0b8c26403392c0b108188d2c56
nghiata-dev <nghiata.dev@gmail.com>
2024-05-30 10:12:04 +0700

    change container content

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
-} from '@material-ui/core/styles';
+} from "@material-ui/core/styles";
 
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co',
+  productionPrefix: "co",
 });
 
 export default () => {
@@ -18,6 +18,7 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
+          <></>
           <MarketingApp />
         </div>
       </StylesProvider>
