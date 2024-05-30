5ba5c538744d796518ed75810c7470ce5e3829a1
nghiata-dev <nghiata.dev@gmail.com>
2024-05-30 10:35:08 +0700

    update code

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App Trigger
+            App
           </Typography>
           <Button
             color="primary"
